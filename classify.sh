1cc1b2c0e93ff8361db9d4da741eb5ae7e8e5656
Arseniy Vershinin <vershinin.arseniy@gmail.com>
2015-12-13 23:20:23 +0100

    Add default classify.sh image

--- classify.sh
+++ classify.sh
@@ -2,4 +2,13 @@
 
 image=$1
 
+if [ -z "$image" ]; then
+        echo "Usage: ./classify.sh path/to/image.png"
+	echo "Classifying image ./images/actors_tests/zoo1.jpg"
+	image="/openface/images/actors_tests/zoo1.jpg"
+fi
+
+# The script uses classifier.py script from the openface repository
+# ./images/actors_features/classifier.pkl is the path to the finetuned
+# model
 ./demos/classifier.py infer ./images/actors_features/classifier.pkl $image
